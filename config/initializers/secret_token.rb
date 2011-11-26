b93f36e37b9ed285db3dd3abb236323674bd3706
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2011-11-27 00:48:21 +0100

    Remove project to start over

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Jsular::Application.config.secret_token = '30d7c2fa4be05c3b08930e6288eeadb0e28e8bf5dee9faff60396f32ccf2c7bc8a82d8fe01ecef4d45c13ab2c7ee9582d4681dfb375f85f8a481876c3a2360b5'
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Jsular::Application.routes.draw do
-
-  resources :expressions, :only => [:index, :show, :create]
-  match '/:id' => 'expressions#show', :as => :permalink
-
-  root :to => 'expressions#index'
-
-end
